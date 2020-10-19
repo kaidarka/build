62a1b0f51c55e8c58f3dabef807e29d15065d2c5
kaidarka <70837397+kaidarka@users.noreply.github.com>
2020-10-20 01:22:48 +0300

    добавил скрипт с скролом и меню

---
+++ js/scroll.js
@@ -0,0 +1,40 @@
+$("#header").click(function() { // ID откуда кливаем
+    $('html, body').animate({
+        scrollTop: $(".header").offset().top  // класс объекта к которому приезжаем
+    }, 1000); // Скорость прокрутки
+    $("#header__menu, #header__burger").removeClass('active');
+});
+$("#projects").click(function() { // ID откуда кливаем
+    $('html, body').animate({
+        scrollTop: $(".projects").offset().top  // класс объекта к которому приезжаем
+    }, 1000); // Скорость прокрутки
+    $("#header__menu, #header__burger").removeClass('active');
+});
+$("#mission").click(function() { // ID откуда кливаем
+    $('html, body').animate({
+        scrollTop: $(".mission").offset().top  // класс объекта к которому приезжаем
+    }, 1000); // Скорость прокрутки
+    $("#header__menu, #header__burger").removeClass('active');
+});
+$("#profit").click(function() { // ID откуда кливаем
+    $('html, body').animate({
+        scrollTop: $(".profit").offset().top  // класс объекта к которому приезжаем
+    }, 1000); // Скорость прокрутки
+    $("#header__menu, #header__burger").removeClass('active');
+});
+$("#contacts").click(function() { // ID откуда кливаем
+    $('html, body').animate({
+        scrollTop: $(".contacts").offset().top  // класс объекта к которому приезжаем
+    }, 1000); // Скорость прокрутки
+    $("#header__menu, #header__burger").removeClass('active');
+});
+$(function() {
+    $(window).scroll(function(){
+        if($(this).scrollTop()>50){
+            $('#navigation').addClass('fixed');
+        }
+        else if ($(this).scrollTop()<140){
+            $('#navigation').removeClass('fixed');
+        }
+    });
+});
